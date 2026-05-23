55f3f52358f5e50135febe51ce482d320924db2a
Leong Jin Zhou <189369351+LeongJinZhou@users.noreply.github.com>
2026-05-23 12:40:06 +0800

    auto: update at 5/23/2026, 12:40:06 PM

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ProgrammeModule } from './programme/programme.module';
 import { CourseModule } from './course/course.module';
 import { TimetableModule } from './timetable/timetable.module';
+import { VenueModule } from './venue/venue.module';
 import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [ProgrammeModule, CourseModule, TimetableModule],
+  imports: [ProgrammeModule, CourseModule, TimetableModule, VenueModule],
   providers: [PrismaService],
 })
 export class AppModule {}
