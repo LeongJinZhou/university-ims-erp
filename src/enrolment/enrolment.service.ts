567d555e23e498aea8961d8411b3957276cd3328
Leong Jin Zhou <189369351+LeongJinZhou@users.noreply.github.com>
2026-05-24 00:04:01 +0800

    auto: update at 5/24/2026, 12:04:01 AM

--- src/enrolment/enrolment.service.ts
+++ src/enrolment/enrolment.service.ts
@@ -140,7 +140,7 @@ export class EnrolmentService {
 
     const existingEnrolments = await this.prisma.enrolment.findMany({
       where: { studentId, isDropped: false },
-      include: { courseOffering: { include: { timetableSlots: true } } },
+      include: { courseOffering: { include: { timetableSlots: true, course: true } } },
     });
 
     for (const enrolment of existingEnrolments) {
