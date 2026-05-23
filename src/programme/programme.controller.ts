6bb72dc2f7387d4d92148fa73f2f6054d60d695b
Leong Jin Zhou <189369351+LeongJinZhou@users.noreply.github.com>
2026-05-23 11:50:24 +0800

    auto: update at 5/23/2026, 11:50:24 AM

---
+++ src/programme/programme.controller.ts
@@ -0,0 +1,70 @@
+import { Controller, Get, Post, Body, Param, Query } from '@nestjs/common';
+import { ProgrammeService } from './programme.service';
+import { CreateDepartmentDto } from './dto/create-department.dto';
+import { CreateFacultyDto } from './dto/create-faculty.dto';
+import { CreateProgrammeDto } from './dto/create-programme.dto';
+import { CreateProgrammeVersionDto } from './dto/create-programme-version.dto';
+import { CreateMqaSemesterPlanDto } from './dto/create-semester-plan.dto';
+
+@Controller('programme')
+export class ProgrammeController {
+  constructor(private readonly programmeService: ProgrammeService) {}
+
+  @Post('departments')
+  createDepartment(@Body() dto: CreateDepartmentDto) {
+    return this.programmeService.createDepartment(dto);
+  }
+
+  @Post('faculties')
+  createFaculty(@Body() dto: CreateFacultyDto) {
+    return this.programmeService.createFaculty(dto);
+  }
+
+  @Post()
+  createProgramme(@Body() dto: CreateProgrammeDto) {
+    return this.programmeService.createProgramme(dto);
+  }
+
+  @Post('versions')
+  createVersion(@Body() dto: CreateProgrammeVersionDto) {
+    return this.programmeService.createProgrammeVersion(dto);
+  }
+
+  @Post('semester-plans')
+  createSemesterPlan(@Body() dto: CreateMqaSemesterPlanDto) {
+    return this.programmeService.createMqaSemesterPlan(dto);
+  }
+
+  @Get('faculties')
+  getAllFaculties() {
+    return this.programmeService.getAllFaculties();
+  }
+
+  @Get('departments')
+  getAllDepartments() {
+    return this.programmeService.getAllDepartments();
+  }
+
+  @Get('faculties/:code/programmes')
+  getProgrammesByFaculty(@Param('code') code: string) {
+    return this.programmeService.getProgrammesByFaculty(code);
+  }
+
+  @Get(':code')
+  getProgramme(@Param('code') code: string) {
+    return this.programmeService.getProgrammeWithVersions(code);
+  }
+
+  @Get('versions/:id/plans')
+  getVersionPlans(@Param('id') id: string) {
+    return this.programmeService.getVersionWithPlans(id);
+  }
+
+  @Get('plans/semester')
+  getSemesterPlan(
+    @Query('semester') semester: number,
+    @Query('versionId') versionId: string,
+  ) {
+    return this.programmeService.getSemesterPlan(semester, versionId);
+  }
+}
