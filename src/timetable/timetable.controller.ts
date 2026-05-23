6d9e5d60bb182458b69f9675bea76a4a15a7b186
Leong Jin Zhou <189369351+LeongJinZhou@users.noreply.github.com>
2026-05-23 12:33:15 +0800

    auto: update at 5/23/2026, 12:33:15 PM

---
+++ src/timetable/timetable.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { TimetableService } from './timetable.service';
+import { CreateSemesterDto } from './dto/create-semester.dto';
+import { CreateCourseOfferingDto } from './dto/create-course-offering.dto';
+import { CreateTimetableSlotDto } from './dto/create-timetable-slot.dto';
+
+@Controller('timetable')
+export class TimetableController {
+  constructor(private readonly timetableService: TimetableService) {}
+
+  @Post('semesters')
+  createSemester(@Body() dto: CreateSemesterDto) {
+    return this.timetableService.createSemester(dto);
+  }
+
+  @Post('offerings')
+  createCourseOffering(@Body() dto: CreateCourseOfferingDto) {
+    return this.timetableService.createCourseOffering(dto);
+  }
+
+  @Post('slots')
+  createTimetableSlot(@Body() dto: CreateTimetableSlotDto) {
+    return this.timetableService.createTimetableSlot(dto);
+  }
+
+  @Post('generate/:semesterId')
+  generateDraftTimetable(@Param('semesterId') semesterId: string) {
+    return this.timetableService.generateDraftTimetable(semesterId);
+  }
+
+  @Post(':id/approve')
+  approveTimetable(@Param('id') id: string, @Body() body: { approverId: string }) {
+    return this.timetableService.approveTimetable(id, body.approverId);
+  }
+}
