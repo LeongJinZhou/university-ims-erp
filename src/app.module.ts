684ea6ea34ae94371c9a5692dca9ce3fc5271d3f
Leong Jin Zhou <189369351+LeongJinZhou@users.noreply.github.com>
2026-05-24 00:02:58 +0800

    auto: update at 5/24/2026, 12:02:58 AM

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import { TimetableModule } from './timetable/timetable.module';
 import { VenueModule } from './venue/venue.module';
 import { StudentModule } from './student/student.module';
 import { ExamModule } from './exam/exam.module';
+import { EnrolmentModule } from './enrolment/enrolment.module';
 import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [ProgrammeModule, CourseModule, TimetableModule, VenueModule, StudentModule, ExamModule],
+  imports: [ProgrammeModule, CourseModule, TimetableModule, VenueModule, StudentModule, ExamModule, EnrolmentModule],
   providers: [PrismaService],
 })
 export class AppModule {}
