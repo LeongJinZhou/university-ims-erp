ff5e99d5c3c214534b5f613cdc75c1ef84ec08df
Leong Jin Zhou <189369351+LeongJinZhou@users.noreply.github.com>
2026-05-24 17:55:12 +0800

    auto: update at 5/24/2026, 5:55:12 PM

--- frontend/src/pages/finance/FinanceDashboard.tsx
+++ frontend/src/pages/finance/FinanceDashboard.tsx
@@ -162,7 +162,7 @@ export function FinanceDashboard() {
 
 function FinanceSkeleton() {
   return (
-    <div className="p-6 space-y-6">
+    <div className="space-y-6">
       <Skeleton className="h-9 w-56" />
       <Skeleton className="h-5 w-96" />
       <Skeleton className="h-24 w-full" />
