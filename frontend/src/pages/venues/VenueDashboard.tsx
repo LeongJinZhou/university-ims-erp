3fc8c28282e2e8ba2f99a4966e71dabb01a9624a
Leong Jin Zhou <189369351+LeongJinZhou@users.noreply.github.com>
2026-05-24 17:46:22 +0800

    auto: update at 5/24/2026, 5:46:22 PM

--- frontend/src/pages/venues/VenueDashboard.tsx
+++ frontend/src/pages/venues/VenueDashboard.tsx
@@ -129,7 +129,7 @@ export function VenueDashboard() {
 
 function VenueSkeleton() {
   return (
-    <div className="p-6 space-y-6">
+    <div className="space-y-6">
       <Skeleton className="h-9 w-72" />
       <Skeleton className="h-5 w-96" />
       <Skeleton className="h-64 w-full" />
