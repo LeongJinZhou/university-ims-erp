819ceb06e45ac35ee4f03d0a1c445fabbe39afd7
Leong Jin Zhou <189369351+LeongJinZhou@users.noreply.github.com>
2026-05-24 17:39:50 +0800

    auto: update at 5/24/2026, 5:39:50 PM

--- frontend/src/pages/students/StudentDashboard.tsx
+++ frontend/src/pages/students/StudentDashboard.tsx
@@ -144,7 +144,7 @@ export function StudentDashboard() {
 
 function StudentSkeleton() {
   return (
-    <div className="p-6 space-y-6">
+    <div className="space-y-6">
       <Skeleton className="h-9 w-64" />
       <Skeleton className="h-5 w-96" />
       <Skeleton className="h-64 w-full" />
