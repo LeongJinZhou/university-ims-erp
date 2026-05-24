7d46209fe285ce7ec8a3fea3f008d9d3b26973c7
Leong Jin Zhou <189369351+LeongJinZhou@users.noreply.github.com>
2026-05-24 13:01:28 +0800

    auto: update at 5/24/2026, 1:01:28 PM

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
-import { Sidebar } from './components/Sidebar'
+import { DashboardLayout } from './components/DashboardLayout'
 import { ProgrammeDashboard } from './pages/programmes/ProgrammeDashboard'
 import { MqaPlans } from './pages/programmes/MqaPlans'
 import { Departments } from './pages/programmes/Departments'
@@ -16,26 +16,23 @@ import { NotificationDashboard } from './pages/notifications/NotificationDashboa
 function App() {
   return (
     <Router>
-      <div className="flex h-screen bg-background">
-        <Sidebar />
-        <main className="flex-1 overflow-auto">
-          <Routes>
-            <Route path="/" element={<Navigate to="/programmes" replace />} />
-            <Route path="/programmes" element={<ProgrammeDashboard />} />
-            <Route path="/programmes/mqa/:id" element={<MqaPlans />} />
-            <Route path="/departments" element={<Departments />} />
-            <Route path="/students" element={<StudentDashboard />} />
-            <Route path="/courses" element={<CourseDashboard />} />
-            <Route path="/timetable" element={<TimetableDashboard />} />
-            <Route path="/venues" element={<VenueDashboard />} />
-            <Route path="/exams" element={<ExamDashboard />} />
-            <Route path="/enrolment" element={<EnrolmentDashboard />} />
-            <Route path="/hr" element={<HrDashboard />} />
-            <Route path="/finance" element={<FinanceDashboard />} />
-            <Route path="/notifications" element={<NotificationDashboard />} />
-          </Routes>
-        </main>
-      </div>
+      <DashboardLayout>
+        <Routes>
+          <Route path="/" element={<Navigate to="/programmes" replace />} />
+          <Route path="/programmes" element={<ProgrammeDashboard />} />
+          <Route path="/programmes/mqa/:id" element={<MqaPlans />} />
+          <Route path="/departments" element={<Departments />} />
+          <Route path="/students" element={<StudentDashboard />} />
+          <Route path="/courses" element={<CourseDashboard />} />
+          <Route path="/timetable" element={<TimetableDashboard />} />
+          <Route path="/venues" element={<VenueDashboard />} />
+          <Route path="/exams" element={<ExamDashboard />} />
+          <Route path="/enrolment" element={<EnrolmentDashboard />} />
+          <Route path="/hr" element={<HrDashboard />} />
+          <Route path="/finance" element={<FinanceDashboard />} />
+          <Route path="/notifications" element={<NotificationDashboard />} />
+        </Routes>
+      </DashboardLayout>
     </Router>
   )
 }
