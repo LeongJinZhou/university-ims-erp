98d39ece0783c387e84a0c7419f188047ef4377e
Leong Jin Zhou <189369351+LeongJinZhou@users.noreply.github.com>
2026-05-25 15:30:57 +0800

    auto: update at 5/25/2026, 3:30:57 PM

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { NotificationController } from './notification.controller';
 import { NotificationService } from './notification.service';
 import { AuditEventListener } from './audit-event.listener';
+import { AnalyticsService } from './analytics.service';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   controllers: [NotificationController],
-  providers: [NotificationService, AuditEventListener, PrismaService],
-  exports: [NotificationService],
+  providers: [NotificationService, AuditEventListener, AnalyticsService, PrismaService],
+  exports: [NotificationService, AnalyticsService],
 })
 export class NotificationModule {}
