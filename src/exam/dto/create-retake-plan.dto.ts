cbfd5c7e11e22bf9c284cb1631d58ec00e5f76b1
Leong Jin Zhou <189369351+LeongJinZhou@users.noreply.github.com>
2026-05-25 10:57:34 +0800

    auto: update at 5/25/2026, 10:57:34 AM

--- src/exam/dto/create-retake-plan.dto.ts
+++ src/exam/dto/create-retake-plan.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, IsNotEmpty } from 'class-validator';
+import { IsString, IsNotEmpty, IsOptional } from 'class-validator';
 
 export class CreateRetakePlanDto {
   @IsString()
@@ -8,4 +8,8 @@ export class CreateRetakePlanDto {
   @IsString()
   @IsNotEmpty()
   projectedGraduation: string;
+
+  @IsString()
+  @IsOptional()
+  resultReleaseId?: string;
 }
