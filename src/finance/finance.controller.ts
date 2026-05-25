8517dc19700d582d92e159fa16c231eb8e98016d
Leong Jin Zhou <189369351+LeongJinZhou@users.noreply.github.com>
2026-05-25 12:21:04 +0800

    auto: update at 5/25/2026, 12:21:04 PM

--- src/finance/finance.controller.ts
+++ src/finance/finance.controller.ts
@@ -1,15 +1,24 @@
 import { Controller, Post, Get, Param, Body } from '@nestjs/common';
 import { FinanceService } from './finance.service';
+import { BillingService } from './billing.service';
 import { GenerateInvoiceDto } from './dto/generate-invoice.dto';
 import { AppealBillingDto } from './dto/appeal-billing.dto';
 
 @Controller('finance')
 export class FinanceController {
-  constructor(private service: FinanceService) {}
+  constructor(
+    private service: FinanceService,
+    private billingService: BillingService,
+  ) {}
 
   @Post('invoices')
   generateInvoice(@Body() dto: GenerateInvoiceDto) {
     return this.service.generateInvoice(dto);
+  }
+
+  @Get('invoices')
+  getAllInvoices() {
+    return this.service.getStudentInvoices('');
   }
 
   @Get('invoices/student/:studentId')
