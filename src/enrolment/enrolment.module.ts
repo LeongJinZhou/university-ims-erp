4d3bf2354fc98930c24cb63894d38a9a8cb5e69b
Leong Jin Zhou <189369351+LeongJinZhou@users.noreply.github.com>
2026-05-25 12:13:28 +0800

    auto: update at 5/25/2026, 12:13:28 PM

--- src/enrolment/enrolment.module.ts
+++ src/enrolment/enrolment.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { EnrolmentController } from './enrolment.controller';
 import { EnrolmentService } from './enrolment.service';
 import { PrismaService } from '../prisma/prisma.service';
+import { FinanceModule } from '../finance/finance.module';
 
 @Module({
+  imports: [FinanceModule],
   controllers: [EnrolmentController],
   providers: [EnrolmentService, PrismaService],
   exports: [EnrolmentService],
