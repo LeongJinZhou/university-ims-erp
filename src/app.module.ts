0d081eb828697df656b28b30b4888980bcbc3cf2
Leong Jin Zhou <189369351+LeongJinZhou@users.noreply.github.com>
2026-05-25 15:31:59 +0800

    auto: update at 5/25/2026, 3:31:59 PM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 import { ProgrammeModule } from './programme/programme.module';
 import { CourseModule } from './course/course.module';
 import { TimetableModule } from './timetable/timetable.module';
 import { VenueModule } from './venue/venue.module';
 import { StudentModule } from './student/student.module';
-import { ExamModule } from './exam/exam.module';
+import { ExamModule } from './exam/ex.module';
 import { EnrolmentModule } from './enrolment/enrolment.module';
 import { HrModule } from './hr/hr.module';
 import { FinanceModule } from './finance/finance.module';
@@ -13,6 +14,7 @@ import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [
+    EventEmitterModule.forRoot(),
     ProgrammeModule, CourseModule, TimetableModule, VenueModule,
     StudentModule, ExamModule, EnrolmentModule, HrModule, FinanceModule,
     NotificationModule,
