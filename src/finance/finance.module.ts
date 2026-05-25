8479aea54ad702b3798ca14db2ac8227083ff20f
Leong Jin Zhou <189369351+LeongJinZhou@users.noreply.github.com>
2026-05-25 12:12:34 +0800

    auto: update at 5/25/2026, 12:12:34 PM

--- src/finance/finance.module.ts
+++ src/finance/finance.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { FinanceController } from './finance.controller';
 import { FinanceService } from './finance.service';
+import { BillingService } from './billing.service';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   controllers: [FinanceController],
-  providers: [FinanceService, PrismaService],
-  exports: [FinanceService],
+  providers: [FinanceService, BillingService, PrismaService],
+  exports: [FinanceService, BillingService],
 })
 export class FinanceModule {}
